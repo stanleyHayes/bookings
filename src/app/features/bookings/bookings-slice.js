e7b8822b274a3cbdb63453f7870ad93d9aaa07df
Stanley Hayford <hayfordstanley@gmail.com>
2021-04-29 23:51:21 +0000

    create booking action creators added in bookings slice

--- src/app/features/bookings/bookings-slice.js
+++ src/app/features/bookings/bookings-slice.js
@@ -1,14 +1,45 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {bookings} from "../../../data/data";
+import axios from "axios";
+import {DEVELOPMENT_SERVER_URL} from "../../../constants/constants";
 
+export const getBookings = createAsyncThunk('bookings/getBookings',
+    async (booking, token) => {
+        await axios(
+            {
+                url: `${DEVELOPMENT_SERVER_URL}/bookings`,
+                method: 'post',
+                data: booking,
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+    });
 const bookingsSlice = createSlice({
     name: "bookings",
     initialState: {
         bookings: [...bookings],
         loading: false,
-        error: null
+        error: null,
+        message: ''
+    },
+    extraReducers: {
+        [getBookings.pending]: (state, action) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [getBookings.fulfilled]: (state, action) =>{
+            state.error = null;
+            state.loading = false;
+            state.bookings.push(action.payload.booking);
+            state.message = action.payload.message
+        },
+        [getBookings.rejected]: (state, action) =>{
+            state.error = action.payload.message;
+            state.loading = false;
+            state.message = action.payload.message
+        }
     }
 });
 
-export const {getBookings, createBooking, updateBooking, deleteBooking} = bookingsSlice.actions;
 export default bookingsSlice.reducer;
