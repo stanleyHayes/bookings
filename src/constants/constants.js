48bc0b3ccf90c5e3b214a36fa8ef5c8b52619cdf
Stanley Hayford <hayfordstanley@gmail.com>
2023-04-12 10:30:09 +0000

    Change server url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
 // export const STREAMING_RESOURCE_GH_SERVER_URL = 'http://localhost:8000/api/v1';
-export const STREAMING_RESOURCE_GH_SERVER_URL = 'https://ghstreamingapp.herokuapp.com/api/v1';
+// export const STREAMING_RESOURCE_GH_SERVER_URL = 'https://ghstreamingapp.herokuapp.com/api/v1';
+export const STREAMING_RESOURCE_GH_SERVER_URL = 'https://streamingresourcesgh.onrender.com/api/v1';
 export const STREAMING_RESOURCE_GH_TOKEN_KEY = 'STREAMING_RESOURCE_GH_TOKEN_KEY';
 export const STREAMING_RESOURCE_GH_USER_KEY = 'STREAMING_RESOURCE_GH_USER_KEY';
